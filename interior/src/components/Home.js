6a24bcd6b042e4cd6bc6d3e801529334c3349872
surendar_m <99544542+Surendar03@users.noreply.github.com>
2024-06-01 18:44:11 +0530

    Update Home.js

--- interior/src/components/Home.js
+++ interior/src/components/Home.js
@@ -27,7 +27,7 @@ function Home() {
             <nav>
         <div class="nav-content">
           <div class="logo">
-            <a href="#">Renoft</a>
+            <NavLink to={"/"}><a href="#">Renoft</a></NavLink>
           </div>
           <ul class="nav-links">
           <li><NavLink to={"/"}><a href="#">Home</a></NavLink></li>
