f7830211ccbe2ef4d845f1df83c51dd01f8727bc
surendar_m <99544542+Surendar03@users.noreply.github.com>
2024-06-01 18:44:46 +0530

    Update Services.js

--- interior/src/components/Services.js
+++ interior/src/components/Services.js
@@ -8,7 +8,7 @@ function Services() {
               <nav>
         <div class="nav-content">
           <div class="logo">
-            <a href="#">Renoft</a>
+            <NavLink to={"/"}><a href="#">Renoft</a></NavLink>
           </div>
           <ul class="nav-links">
           <li><NavLink to={"/"}><a href="#">Home</a></NavLink></li>
