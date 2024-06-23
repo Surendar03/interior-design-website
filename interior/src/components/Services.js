7bf68fb57fa8eb6b3c27cf97948e36c4ff2a90f9
surendar_m <99544542+Surendar03@users.noreply.github.com>
2024-06-23 11:51:37 +0530

    Update Services.js

--- interior/src/components/Services.js
+++ interior/src/components/Services.js
@@ -1,27 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { NavLink } from "react-router-dom";
 import '../App.css';
-import { NavLink } from 'react-router-dom'
+import { useLocation } from 'react-router-dom';
 
 function Services() {
+  const { hash } = useLocation();
+
+  useEffect(() => {
+    if (hash) {
+      const element = document.getElementById(hash.replace('#', ''));
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [hash]);
     return (
-        <div>
+        <div id="body">
               <nav>
-        <div class="nav-content">
+        <div class="nav-content" id="navbar1">
           <div class="logo">
-            <NavLink to={"/"}><a href="#">Renoft</a></NavLink>
+          <NavLink to={"/"}><a href="#">Renoft</a></NavLink>
           </div>
           <ul class="nav-links">
-          <li><NavLink to={"/"}><a href="#">Home</a></NavLink></li>
-            <li><NavLink to={"/Services"}><a href="#">Services</a></NavLink></li>
-            <li><NavLink to={"/Contact"}><a href="#">Contact</a></NavLink></li>
-            <li><NavLink to={"/Aboutus"}><a href="#">About Us</a></NavLink></li>
-            <li><NavLink to={"/Projects"}><a href="#">Projects</a></NavLink></li>
-            <li><NavLink to={"/FAQs"}><a href="#">FAQs</a></NavLink></li>
-
+          <li><NavLink to={"/#body"}><a href="#">Home</a></NavLink></li>
+            <li><NavLink to={"/Services#body"}><a href="#">Services</a></NavLink></li>
+            <li><NavLink to={"/Contact#body"}><a href="#">Contact</a></NavLink></li>
+            <li><NavLink to={"/Aboutus#body"}><a href="#">About Us</a></NavLink></li>
+            <li><NavLink to={"/Projects#body"}><a href="#">Projects</a></NavLink></li>
+            <li><NavLink to={"/FAQs#body"}><a href="#">FAQs</a></NavLink></li>
           </ul>
         </div>
       </nav>
-      <div className='top-border'></div>
+      {/* <div className='top-border'id="topborder"></div> */}
       <div className='projecthead'></div>
 <p className='service-para'>FIND EVERYTHING FOR YOUR HOME, RIGHT HERE AT CHATTELS DESIGN</p>
 <h1 className='service-h1'>Suite of Services for You!</h1>
@@ -71,12 +81,12 @@ function Services() {
     <div class="footer-left">
       
       <ul class="footer-links">
-      <li><NavLink to={"/"}><a href="#">Home</a></NavLink></li>
-      <li><NavLink to={"/Services"}><a href="#">Services</a></NavLink></li>
-            <li><NavLink to={"/Contact"}><a href="#">Contact</a></NavLink></li>
-            <li><NavLink to={"/Aboutus"}><a href="#">About Us</a></NavLink></li>
-            <li><NavLink to={"/Projects"}><a href="#">Projects</a></NavLink></li>
-            <li><NavLink to={"/FAQs"}><a href="#">FAQs</a></NavLink></li>
+      <li><NavLink to={"/#body"}><a href="#">Home</a></NavLink></li>
+            <li><NavLink to={"/Services#body"}><a href="#">Services</a></NavLink></li>
+            <li><NavLink to={"/Contact#body"}><a href="#">Contact</a></NavLink></li>
+            <li><NavLink to={"/Aboutus#body"}><a href="#">About Us</a></NavLink></li>
+            <li><NavLink to={"/Projects#body"}><a href="#">Projects</a></NavLink></li>
+            <li><NavLink to={"/FAQs#body"}><a href="#">FAQs</a></NavLink></li>
       </ul>
     </div>
   </div>
