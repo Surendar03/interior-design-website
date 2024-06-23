4c9d823b2af2799af8ad60ac69a5d90ff505290d
surendar_m <99544542+Surendar03@users.noreply.github.com>
2024-06-23 11:52:08 +0530

    Update Aboutus.js

--- interior/src/components/Aboutus.js
+++ interior/src/components/Aboutus.js
@@ -1,31 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import '../App.css';
 import { NavLink } from 'react-router-dom'
+import { useLocation } from 'react-router-dom';
 import abt3 from '../assets/designphotos/livingroom2.jpg'
 import abt1 from '../assets/minimalist-home-office-in-bedroom.jpg'
 import abt2 from '../assets/services-banner-1-1.jpg'
 import renoftlogo from '../assets/renoft-logo.png'
 
 function Aboutus() {
+  const { hash } = useLocation();
+
+  useEffect(() => {
+    if (hash) {
+      const element = document.getElementById(hash.replace('#', ''));
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [hash]);
     return (
-        <div>
+        <div id="body">
              <nav>
         <div class="nav-content">
           <div class="logo">
-            <NavLink to={"/"}><a href="#">Renoft</a></NavLink>
+          <NavLink to={"/#body"}><a href="#">Renoft</a></NavLink>
           </div>
           <ul class="nav-links">
-          <li><NavLink to={"/"}><a href="#">Home</a></NavLink></li>
-          <li><NavLink to={"/Services"}><a href="#">Services</a></NavLink></li>
-            <li><NavLink to={"/Contact"}><a href="#">Contact</a></NavLink></li>
-            <li><NavLink to={"/Aboutus"}><a href="#">About Us</a></NavLink></li>
-            <li><NavLink to={"/Projects"}><a href="#">Projects</a></NavLink></li>
-            <li><NavLink to={"/FAQs"}><a href="#">FAQs</a></NavLink></li>
+          <li><NavLink to={"/#body"}><a href="#">Home</a></NavLink></li>
+          <li><NavLink to={"/Services#body"}><a href="#">Services</a></NavLink></li>
+            <li><NavLink to={"/Contact#body"}><a href="#">Contact</a></NavLink></li>
+            <li><NavLink to={"/Aboutus#body"}><a href="#">About Us</a></NavLink></li>
+            <li><NavLink to={"/Projects#body"}><a href="#">Projects</a></NavLink></li>
+            <li><NavLink to={"/FAQs#body"}><a href="#">FAQs</a></NavLink></li>
 
           </ul>
         </div>
       </nav>
-      <div className='top-border'></div>
+      {/* <div className='top-border'></div> */}
       <div className='aboutusheadimg'>
         <img src={renoftlogo} alt="headimg"/>
       </div>
@@ -80,12 +91,12 @@ In conclusion, Renoft is more than just an interior design service provider; we'
     <div class="footer-left">
       
       <ul class="footer-links">
-      <li><NavLink to={"/"}><a href="#">Home</a></NavLink></li>
-      <li><NavLink to={"/Services"}><a href="#">Services</a></NavLink></li>
-            <li><NavLink to={"/Contact"}><a href="#">Contact</a></NavLink></li>
-            <li><NavLink to={"/Aboutus"}><a href="#">About Us</a></NavLink></li>
-            <li><NavLink to={"/Projects"}><a href="#">Projects</a></NavLink></li>
-            <li><NavLink to={"/FAQs"}><a href="#">FAQs</a></NavLink></li>
+      <li><NavLink to={"/#body"}><a href="#">Home</a></NavLink></li>
+            <li><NavLink to={"/Services#body"}><a href="#">Services</a></NavLink></li>
+            <li><NavLink to={"/Contact#body"}><a href="#">Contact</a></NavLink></li>
+            <li><NavLink to={"/Aboutus#body"}><a href="#">About Us</a></NavLink></li>
+            <li><NavLink to={"/Projects#body"}><a href="#">Projects</a></NavLink></li>
+            <li><NavLink to={"/FAQs#body"}><a href="#">FAQs</a></NavLink></li>
       </ul>
     </div>
   </div>
